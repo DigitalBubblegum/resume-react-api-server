3c68b8fbff3b4e4ef12bf9d945bf944f4b7a8584
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2024-01-09 20:06:56 +0530

    -created a simple v1 version of an endpoint

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-import logger  from './utils/logger'
-logger.info('hello world')
+const express = require('express')
+require('dotenv').config()
+const app = express()
+app.get('/v1/',(request,response) => {
+    response.send({info:'This is the v1 version of the endpoint'})
+})
+const PORT = process.env.PORT
+app.listen(PORT,()=>[
+    console.log(`Server running on port ${PORT}`)
+])
